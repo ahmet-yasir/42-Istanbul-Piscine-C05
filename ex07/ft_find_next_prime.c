0a3503a75d763e3f27165e54d9ab599cd2234860
Ahmet Yasir Kulaksiz <akulaksi@k1m21s08.42istanbul.com.tr>
2024-02-13 13:58:51 +0300

    update ex06-ex07

--- ex07/ft_find_next_prime.c
+++ ex07/ft_find_next_prime.c
@@ -6,7 +6,7 @@
 /*   By: akulaksi <akulaksi@student.42istanbul.c    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/13 13:45:47 by akulaksi          #+#    #+#             */
-/*   Updated: 2024/02/13 13:49:22 by akulaksi         ###   ########.fr       */
+/*   Updated: 2024/02/13 13:58:12 by akulaksi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,9 @@ int	ft_is_prime(int nb)
 
 int	ft_find_next_prime(int nb)
 {
-	while (nb < 2147483647 && ft_is_prime(nb) != 0)
+	if (nb < 2)
+		return (2);
+	while (nb < 2147483647 && ft_is_prime(nb) == 0)
 		nb++;
 	return (nb);
 }
