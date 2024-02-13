a2da24993aaa3182d25167dbccfc8de30ac55499
ahmet-yasir <131553807+ahmet-yasir@users.noreply.github.com>
2024-02-13 17:36:03 +0300

    Update ft_iterative_factorial.c

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_recursive_factorial(int nb)
+int	ft_iterative_factorial(int nb)
 {
 	unsigned int	fact;
 
